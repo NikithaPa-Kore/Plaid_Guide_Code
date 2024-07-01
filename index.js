ea6d4418988c8f86bc7db7bd6e9a2d5d9e755f87
NikithaPa-Kore <150217564+NikithaPa-Kore@users.noreply.github.com>
2024-07-01 15:51:05 +0530

    Update index.js
    Changed the port number to the default Port.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const bodyParser = require('body-parser');
 const moment = require('moment');
 const cors = require('cors');
 
-const APP_PORT = process.env.APP_PORT || 8008;
+const APP_PORT = process.env.APP_PORT || 8000;
 const PLAID_CLIENT_ID = process.env.PLAID_CLIENT_ID;
 const PLAID_SECRET = process.env.PLAID_SECRET;
 const PLAID_ENV = process.env.PLAID_ENV || 'sandbox';
